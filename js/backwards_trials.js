4cfd64bf66a1a4242f76fb767d32c79c696c172f
Baaple <35265836+Baaple@users.noreply.github.com>
2020-08-26 10:20:43 +0100

    Add files via upload

---
+++ js/backwards_trials.js
@@ -0,0 +1,98 @@
+var backwards_trials = [
+ {
+   task: "backwards_practice",
+   span: 2,
+   seq_num: 1,
+   seq: [5,9]
+ },
+ {
+   task: "backwards_practice",
+   span: 2,
+   seq_num: 2,
+   seq: [6,1]
+ },
+ {
+   task: "backwards_main",
+   span: 2,
+   seq_num: 1,
+   seq: [2,4]
+ },
+ {
+   task: "backwards_main",
+   span: 2,
+   seq_num: 2,
+   seq: [5,7]
+ },
+ {
+   task: "backwards_main",
+   span: 3,
+   seq_num: 1,
+   seq: [6,2,9]
+ },
+ {
+   task: "backwards_main",
+   span: 3,
+   seq_num: 2,
+   seq: [4,1,5]
+ },
+ {
+   task: "backwards_main",
+   span: 4,
+   seq_num: 1,
+   seq: [3,2,7,9]
+ },
+ {
+   task: "backwards_main",
+   span: 4,
+   seq_num: 2,
+   seq: [4,9,6,8]
+ },
+ {
+   task: "backwards_main",
+   span: 5,
+   seq_num: 1,
+   seq: [1,5,2,8,6]
+ },
+ {
+   task: "backwards_main",
+   span: 5,
+   seq_num: 2,
+   seq: [6,1,8,4,3]
+ },
+ {
+   task: "backwards_main",
+   span: 6,
+   seq_num: 1,
+   seq: [5,3,9,4,1,8]
+ },
+ {
+   task: "backwards_main",
+   span: 6,
+   seq_num: 2,
+   seq: [7,2,4,8,5,6]
+ },
+ {
+   task: "backwards_main",
+   span: 7,
+   seq_num: 1,
+   seq: [8,1,2,9,3,6,5]
+ },
+ {
+   task: "backwards_main",
+   span: 7,
+   seq_num: 2,
+   seq: [4,7,3,9,1,2,8]
+ },
+ {
+   task: "backwards_main",
+   span: 8,
+   seq_num: 1,
+   seq: [9,4,3,7,6,2,5,8]
+ },
+ {
+   task: "backwards_main",
+   span: 8,
+   seq_num: 2,
+   seq: [7,2,8,1,9,6,5,3]
+ },
+];
---
+++ js/forwards_trials.js
@@ -0,0 +1,110 @@
+var forwards_trials = [
+ {
+   task: "forwards_practice",
+   span: 3,
+   seq_num: 1,
+   seq: [5,9,4]
+ },
+ {
+   task: "forwards_practice",
+   span: 3,
+   seq_num: 2,
+   seq: [8,1,3]
+ },
+ {
+   task: "forwards_main",
+   span: 2,
+   seq_num: 1,
+   seq: [1,7]
+ },
+ {
+   task: "forwards_main",
+   span: 2,
+   seq_num: 2,
+   seq: [6,3]
+ },
+ {
+   task: "forwards_main",
+   span: 3,
+   seq_num: 1,
+   seq: [5,8,2]
+ },
+ {
+   task: "forwards_main",
+   span: 3,
+   seq_num: 2,
+   seq: [6,9,4]
+ },
+ {
+   task: "forwards_main",
+   span: 4,
+   seq_num: 1,
+   seq: [6,4,3,9]
+ },
+ {
+   task: "forwards_main",
+   span: 4,
+   seq_num: 2,
+   seq: [7,2,8,6]
+ },
+ {
+   task: "forwards_main",
+   span: 5,
+   seq_num: 1,
+   seq: [4,2,7,3,1]
+ },
+ {
+   task: "forwards_main",
+   span: 5,
+   seq_num: 2,
+   seq: [7,5,8,3,6]
+ },
+  {
+   task: "forwards_main",
+   span: 6,
+   seq_num: 1,
+   seq: [6,1,9,4,7,3]
+ },
+  {
+   task: "forwards_main",
+   span: 6,
+   seq_num: 2,
+   seq: [3,9,2,4,8,7]
+ },
+  {
+   task: "forwards_main",
+   span: 7,
+   seq_num: 1,
+   seq: [5,9,1,7,4,2,8]
+ },
+  {
+   task: "forwards_main",
+   span: 7,
+   seq_num: 2,
+   seq: [4,1,7,9,3,8,6]
+ },
+  {
+   task: "forwards_main",
+   span: 8,
+   seq_num: 1,
+   seq: [5,8,1,9,2,6,4,7]
+ },
+  {
+   task: "forwards_main",
+   span: 8,
+   seq_num: 2,
+   seq: [3,8,2,9,5,1,7,4]
+ },
+  {
+   task: "forwards_main",
+   span: 9,
+   seq_num: 1,
+   seq: [2,7,5,8,6,2,5,8,4]
+ },
+  {
+   task: "forwards_main",
+   span: 9,
+   seq_num: 2,
+   seq: [7,1,3,9,4,2,5,6,8]
+ },
+];
